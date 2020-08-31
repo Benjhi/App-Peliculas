894d6f1f68dcd6d68df6ba420cc1eb140e49fa3e
benjhi <renzo_soler@hotmail.com>
2020-08-31 07:20:30 -0300

    Renzo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom'
 
 import List from './containers/List'
 
-ReactDOM.render(<List/>, document.getElementById('root'))
+import 'bootswatch/dist/darkly/bootstrap.min.css';
+
+const App = () => {
+    return (
+     <Fragment>
+      <nav className="navbar navbar-white bg-dark border-bottom border-white">
+        <a href="/" className="navbar-brand text-white">
+          MovieApp
+        </a>
+      </nav>
+      <main className="bg-dark">
+        <div className="container">
+          <List />
+        </div>
+      </main>
+    </Fragment>
+    );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
